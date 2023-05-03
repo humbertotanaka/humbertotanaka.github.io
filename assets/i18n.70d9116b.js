6a6d2ec86fea3b4a28ad1902b800fcbc58bac4ea
Humberto Tanaka <humberto.skaa@hotmail.com.br>
2023-05-03 00:52:46 -0300

    Updates

---
+++ assets/i18n.70d9116b.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.cf48ea01.js";import{i as r}from"./index.ca9432cd.js";var s=a(({app:o})=>{o.use(r)});export{s as default};
--- assets/i18n.ad3263c2.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.97242b20.js";import{i as r}from"./index.e6c11bb8.js";var s=a(({app:o})=>{o.use(r)});export{s as default};
